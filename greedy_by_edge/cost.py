e8d0066e0cfe72681d54649f9e7a61f0ab77e579
louisjyli <louisjyli@gmail.com>
2017-01-15 06:07:35 +0800

    project created

---
+++ greedy_by_edge/cost.py
@@ -0,0 +1,29 @@
+__author__ = 'louisjyli'
+
+def compute_cost(file_name, dict_cluster):
+    edge_set = set()
+    graph=open(file_name,'r+')
+    TP = 0
+    TF = 0
+    for a_line in graph:
+        an_edge = a_line.split(" ")
+        s_node = an_edge[0]
+        d_node = an_edge[1]
+        edge_set.add(s_node+' '+d_node)
+    for node1 in dict_cluster.keys():
+        for node2 in dict_cluster.keys():
+            if node1 != node2:
+                checking_edge = node1 + ' ' + node2
+                if checking_edge in edge_set and dict_cluster[node1]==dict_cluster[node2]:
+                    TP += 1
+                elif checking_edge not in edge_set and dict_cluster[node1]!=dict_cluster[node2]:
+                    TF += 1
+                else:
+                    a = 0
+            else:
+                a = 0
+    number_of_edges = len(dict_cluster)*(len(dict_cluster)-1)
+    cost = number_of_edges - TP - TF
+    agreement = TP + TF
+    gain = agreement - cost
+    return cost, agreement, gain
---
+++ greedy_by_edge/greedy_split.py
@@ -0,0 +1,27 @@
+__author__ = 'louisjyli'
+file_name = 'CollegeMsg.txt'
+file=open(file_name,'r+')
+from cost import compute_cost
+
+dict_cluster = {}
+cluster_label = 0
+
+for line in file:
+    edge = line.split(' ')
+    source_node = edge[0]
+    dest_node = edge[1]
+    if source_node not in dict_cluster and dest_node not in dict_cluster:
+        dict_cluster[source_node] = cluster_label
+        dict_cluster[dest_node] = cluster_label
+        cluster_label += 1
+    elif source_node in dict_cluster and dest_node not in dict_cluster:
+        dict_cluster[dest_node] = cluster_label
+        cluster_label += 1
+    elif source_node not in dict_cluster and dest_node in dict_cluster:
+        dict_cluster[source_node] = cluster_label
+        cluster_label += 1
+    else:
+        a = 0
+
+performance = compute_cost(file_name,dict_cluster)
+print('(Cost, Agreement, Gain) : ' + str(performance))
